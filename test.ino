50bf65a07fdf804d8db496d20587ee5113326193
rotyan <rotyan@foxmail.com>
2021-04-07 22:27:41 +0800

    21.4.7

--- test.ino
+++ test.ino
@@ -1,5 +1,7 @@
 
-//ctrl+alt_u实现编译和运行
+// Ctrl+Alt+U 编译并上传(运行)
+// Ctrl+Alt+R 进行编译验证
+// 烧录：简单的说就是让微型计算机开机能执行你的程序的过程就是程序烧录。
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);

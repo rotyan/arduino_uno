e3fa3ce82d38932ba8687ea22880628eedfe2e4d
rotyan <rotyan@foxmail.com>
2021-04-09 16:12:26 +0800

    21.4.9

--- LED.ino
+++ LED.ino
@@ -2,18 +2,22 @@
 
 int yellow = 13;
 int green = 12;
+int red = 11;
 
 void setup() {
     pinMode(green, OUTPUT);
     pinMode(yellow, OUTPUT);
-    
+    pinMode(red, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(green, HIGH);
-    delay(2000);
     digitalWrite(yellow, HIGH);
+    digitalWrite(yellow, LOW);
+    delay(2000);
+    digitalWrite(green, HIGH);
     digitalWrite(green, LOW);
     delay(2000);
-    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    digitalWrite(red, LOW);
+    delay(2000);
 }
